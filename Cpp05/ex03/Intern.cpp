c33f4391946b84c581d7fab0af6638033fde00ad
Spike-hd <hadrien.duflos@outlook.fr>
2025-05-09 14:10:47 +0200

    correction 3

--- Cpp05/ex03/Intern.cpp
+++ Cpp05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: spike <spike@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 11:42:18 by spike             #+#    #+#             */
-/*   Updated: 2025/05/09 13:06:21 by spike            ###   ########.fr       */
+/*   Updated: 2025/05/09 14:10:21 by spike            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,34 @@ Intern::Intern(Intern const &other) {(void)other;}
 Intern	&Intern::operator=(Intern const &other) {(void)other; return *this;}
 Intern::~Intern() {}
 
+
+
 AForm	*Intern::makeForm(const std::string &FormName, const std::string &target)
 {
-	if (FormName == "ShrubberyCreationForm")
+	int j = -1;
+	std::string AllNames[3] = {"shrubbery creation", "robotomy request", "presidential pardon"};
+
+	AForm *forms[3] = {
+		new ShrubberyCreationForm(target),
+		new RobotomyRequestForm(target),
+		new PresidentialPardonForm(target)
+	};
+
+	for (int i = 0; i < 3; ++i)
 	{
-		std::cout << "Intern prints " << FormName << std::endl;
-		return new ShrubberyCreationForm(target);
+		if (FormName == AllNames[i])
+		{
+			std::cout << "Intern has created " << FormName << std::endl;
+			j = i;
+		}
+		else
+		 delete forms[i];
 	}
-	else if (FormName == "RobotomyRequestForm")
+	if (j == -1)
 	{
-		std::cout << "Intern prints " << FormName << std::endl;
-		return new RobotomyRequestForm(target);
-	}
-	else if (FormName == "PresidentialPardonForm")
-	{
-		std::cout << "Intern prints " << FormName << std::endl;
-		return new PresidentialPardonForm(target);
-	}
-	else
-	{
-		std::cerr << "Error: unknown form name \"" << FormName << "\"" << std::endl;
+		std::cout << "Intern couldn't create the file " << FormName << std::endl;
 		return (NULL);
 	}
+
+	return forms[j];
 }
